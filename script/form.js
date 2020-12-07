f3a48a3a20b2243dc433ba88f4900deea820f64f
Stefan <stefflr@yahoo.com>
2020-12-07 10:24:12 +0100

    created side-bar/ functionality for order btns

--- script/form.js
+++ script/form.js
@@ -12,6 +12,33 @@ document.querySelectorAll("li").forEach(btn => btn.addEventListener("click", cha
 document.querySelectorAll(".beer-text-wrapper .primary-btn").forEach(btn => btn.addEventListener("click", learnMore));
 document.querySelectorAll("input[type=radio]").forEach(btn => {btn.addEventListener("click", setActivePaymentOption)
 console.log(btn)});
+document.querySelectorAll("#side-menu").forEach(btn => btn.addEventListener("click", toggleSideMenu));
+
+const orderBtns = document.querySelectorAll(".order-btn-wrapper");
+orderBtns.forEach(btn => btn.addEventListener("click", orderBeer));
+
+function orderBeer(e){
+    let orderAmount = e.target.parentNode.querySelector(".order-amount");
+
+    if(e.target.textContent == "+"){
+        orderAmount.textContent = parseInt(orderAmount.textContent) + 1;
+    } else if (e.target.textContent == "-") {
+        if(parseInt(orderAmount.textContent)>=1)
+        orderAmount.textContent = parseInt(orderAmount.textContent) - 1;
+    }
+}
+
+
+
+
+
+
+
+
+
+function toggleSideMenu(){
+    document.querySelector(".side-menu").classList.toggle("extended");
+}
 
 function setActivePaymentOption(e){
     console.log(e.target);
@@ -23,6 +50,8 @@ function changeSection(e) {
 
     activeSection = e.target.id.split("-")[0];
 
+    if(document.querySelector(".side-menu").classList.contains("extended"))
+        document.querySelector(".side-menu").classList.toggle("extended")
 
     document.querySelectorAll("main > section").forEach(section => {
         if (section.id == activeSection) {
